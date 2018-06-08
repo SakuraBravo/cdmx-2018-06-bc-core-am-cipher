9d986dd808b134f7cef50780106f7a27cfa7003e
Sakura Bravo <SakuraBravo@MacBook-Pro-de-Nallely.local>
2018-06-08 08:58:18 -0500

    Primera entrega del Código Cesar

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//function introducirTexto(){ //ES5
+//    let x = document.getElementById("entrada").value;
+//    let y = document.getElementById("offset").value;
+//    document.getElementById("demo").innerHTML = x + " " + y;
+//}
+const introducirTexto = () => { // Sintaxis ES6
+  const intro = document.getElementById("entrada").value; // Declaro la variable como var porque necesito poder acceder a ella por fuera de la función
+  const offset = document.getElementById("offset").value;
+  document.getElementById("demo").innerHTML = intro + " " + offset; // Lo hice para provar
+  if (true){
+   let i = 0;// Esta es la funcion que mide el string y lo separa en índices
+    while (i < intro.length) {
+      var altas = (((intro[i]).toUpperCase())).charCodeAt();//Variable con codigo ASCII
+      //console.log(altas); // Compruebo que imprima en la pantalla
+      i++
+   const codificar = (x , y) => { //Declaro la variable para guardar lo obtenido en la funcion que codifica el string
+       return (x - 65 + y) % 26 + 65;
+       }
+   const codigo = codificar (altas, offset); //Declaro los argumentos que se aplican a la función que codifica el mensaje
+   const respuesta = String.fromCharCode(codigo);// Aplico el método de
+   console.log(respuesta);
+   document.getElementById("demo2").innerHTML += respuesta;
+    }
+  //for (let i=0; i< intro.length; i++){ //For para separar el string si sale.
+  //console.log(intro.substr(i,1));
+  //}
+  }
+}
